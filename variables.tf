e8fec53886843793079a70c6e0e3ddb50867e0f6
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 09:30:03 -0700

    Add new variables for SQL and locations

--- variables.tf
+++ variables.tf
@@ -1,15 +1,29 @@
-variable "prefix" {
-  type    = string
-  default = "fcha"
-}
-
-variable "primary_region" {
+variable "primary_location" {
   type    = string
   default = "canadacentral"
 }
 
-variable "secondary_region" {
+variable "secondary_location" {
   type    = string
-  default = "eastus"
+  default = "eastus2"
 }
 
+variable "project" {
+  type    = string
+  default = "fcha-ha"
+}
+
+variable "sql_admin_username" {
+  type    = string
+  default = "sqladminuser"
+}
+
+variable "sql_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "enable_sql_secondary" {
+  type    = bool
+  default = false
+}
