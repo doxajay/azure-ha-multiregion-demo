d58ed6a5645388d6be3d2bd24450e54369aac565
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 08:16:14 -0700

    Add Terraform cloud configuration and providers
    Configure Terraform cloud settings and required providers.

--- versions.tf
+++ versions.tf
@@ -1,1 +1,25 @@
+terraform {
+  cloud {
+    organization = "POV-1"
+    workspaces {
+      name = "azure-ha-multiregion-demo"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.110"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 
