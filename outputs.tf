d946c2f30930b808131ac11a09c4007f18cc5c0d
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 09:31:17 -0700

    Refactor output variable names and add new output
    Renamed output variables and added sql_primary_server output.

--- outputs.tf
+++ outputs.tf
@@ -6,12 +6,10 @@ output "secondary_app_url" {
   value = "https://${azurerm_linux_web_app.app_secondary.default_hostname}"
 }
 
-output "front_door_url" {
+output "frontdoor_url" {
   value = "https://${azurerm_cdn_frontdoor_endpoint.afd_endpoint.host_name}"
 }
 
-output "sql_failover_group_listener" {
-  value = azurerm_mssql_failover_group.fg.read_write_endpoint_failover_policy[0].mode
-  description = "Failover group created; connect apps to the FG listener (shown in Azure portal under failover group)."
+output "sql_primary_server" {
+  value = azurerm_mssql_server.sql_primary.fully_qualified_domain_name
 }
-
