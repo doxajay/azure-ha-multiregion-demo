f837879b440ee1bf4d935fafa906f1511ebfcdd5
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 08:16:40 -0700

    Add variables for region and prefix configuration

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "prefix" {
+  type    = string
+  default = "fcha"
+}
+
+variable "primary_region" {
+  type    = string
+  default = "canadacentral"
+}
+
+variable "secondary_region" {
+  type    = string
+  default = "canadaeast"
+}
 
